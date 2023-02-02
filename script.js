af6e21ff6b9480d64e88da33075008baf736aa07
peter <peterleite.dev@gmail.com>
2023-02-02 17:51:10 -0300

    header mobile ready

--- script.js
+++ script.js
@@ -10,5 +10,34 @@ function sitckyHeader() {
   });
 }
 
+function activeHamburger() {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.header-links');
+  const navLinks = document.querySelectorAll('.header-links li');
 
-sitckyHeader();
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("hamburger-active");
+
+    navLinks.forEach((link, index) => {
+      if(link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `linksFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+    //Burger animation
+    hamburger.classList.toggle("toggle");
+
+
+  });
+
+
+}
+
+
+const app = ()=> {
+  activeHamburger();
+  sitckyHeader();
+}
+
+app();
