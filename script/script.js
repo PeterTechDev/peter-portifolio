0932901e9b02692989209442600862b84145d8a7
peter <peterleite.dev@gmail.com>
2023-05-05 06:12:33 -0300

    create darkmode btn without the script

--- script/script.js
+++ script/script.js
@@ -1,43 +1,50 @@
 
-const header = document.querySelector("header");
+// const header = document.querySelector("header");
 
 
-function sitckyHeader() { 
-  let navOffset = header.offsetTop + 10;
+// function sitckyHeader() { 
+//   let navOffset = header.offsetTop + 10;
 
-  window.addEventListener("scroll", () => {
-    (window.scrollY >= navOffset) ? header.classList.add("sticky") : header.classList.remove("sticky")
-  });
-}
+//   window.addEventListener("scroll", () => {
+//     (window.scrollY >= navOffset) ? header.classList.add("sticky") : header.classList.remove("sticky")
+//   });
+// }
 
-function activeHamburger() {
-  const hamburger = document.querySelector('.hamburger');
-  const nav = document.querySelector('.header-links');
-  const navLinks = document.querySelectorAll('.header-links li');
+// function activeHamburger() {
+//   const hamburger = document.querySelector('.hamburger');
+//   const nav = document.querySelector('.header-links');
+//   const navLinks = document.querySelectorAll('.header-links li');
 
-  hamburger.addEventListener("click", () => {
-    nav.classList.toggle("hamburger-active");
+//   hamburger.addEventListener("click", () => {
+//     nav.classList.toggle("hamburger-active");
 
-    navLinks.forEach((link, index) => {
-      if(link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `linksFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-      }
-    });
-    //Burger animation
-    hamburger.classList.toggle("toggle");
-
-
-  });
+//     navLinks.forEach((link, index) => {
+//       if(link.style.animation) {
+//         link.style.animation = "";
+//       } else {
+//         link.style.animation = `linksFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+//       }
+//     });
+//     //Burger animation
+//     hamburger.classList.toggle("toggle");
 
 
-}
+//   });
+// }
 
 
-const app = ()=> {
-  activeHamburger();
-  sitckyHeader();
-}
+// const app = ()=> {
+//   activeHamburger();
+//   sitckyHeader();
+// }
 
-app();
+// app();
+
+
+// DARK MODE BUTTOn
+
+const darkModeButton = document.querySelector('#darkmode-btn');
+
+darkModeButton.addEventListener('click', function() {
+  console.log("aiaiiaiaia");
+});
