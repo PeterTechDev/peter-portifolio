b9d6ccefc09a0fc89a8657e62dc89cb8ef3fd86c
peter <peterleite.dev@gmail.com>
2023-05-29 00:10:09 -0300

    add modal form sent

--- script/contact.js
+++ script/contact.js
@@ -9,3 +9,20 @@ function blurInput(input) {
 console.log("contact.js is loaded");
 
 export { blurInput };
+
+document.querySelector('form[name="contact"]').addEventListener('submit', function (event) {
+  event.preventDefault(); // Impede o envio padrão do formulário
+
+  // Exibe a modal
+  document.getElementById('modal').style.display = 'block';
+});
+
+document.getElementById('modal').addEventListener('click', closeModal);
+
+function closeModal(event) {
+    if (event.target === this) {
+      // Oculta a modal
+      this.style.display = 'none';
+    }
+}
+
