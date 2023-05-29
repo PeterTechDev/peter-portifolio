8e28b6898417b416dae823afb2ba928961dfa8d1
peter <peterleite.dev@gmail.com>
2023-05-29 00:29:15 -0300

    testing netily form #3 script at html

--- script/contact.js
+++ script/contact.js
@@ -10,12 +10,12 @@ console.log("contact.js is loaded");
 
 export { blurInput };
 
-document.querySelector('form[name="contact-netlify"]').addEventListener('submit', function (event) {
-  event.preventDefault(); // Impede o envio padrão do formulário
+// document.querySelector('form[name="contact-netlify"]').addEventListener('submit', function (event) {
+//   event.preventDefault(); // Impede o envio padrão do formulário
 
-  // Exibe a modal
-  document.getElementById('modal').style.display = 'block';
-});
+//   // Exibe a modal
+//   document.getElementById('modal').style.display = 'block';
+// });
 
 document.getElementById('modal').addEventListener('click', closeModal);
 
@@ -24,5 +24,33 @@ function closeModal(event) {
       // Oculta a modal
       this.style.display = 'none';
     }
+}
+
+function submitForm(event) {
+  event.preventDefault(); // Impede o envio padrão do formulário
+  console.log("submitForm");
+
+  // Envio do formulário para o Netlify
+  var form = event.target;
+  var formData = new FormData(form);
+  fetch(form.action, {
+    method: 'POST',
+    body: formData,
+  })
+    .then(function(response) {
+      // Exibe a modal após o envio bem-sucedido do formulário
+      document.getElementById('modal').style.display = 'block';
+
+      // Aqui você pode adicionar qualquer lógica adicional que deseja realizar após o envio do formulário
+      document.getElementById('modal').style.display = 'block';
+
+      // Por exemplo, você pode limpar os campos do formulário
+      form.reset();
+      console.log("submitForm");
+
+    })
+    .catch(function(error) {
+      console.error('Erro ao enviar o formulário:', error);
+    });
 }
 
